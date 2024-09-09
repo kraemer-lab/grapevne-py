5f0cab3b3bbc34c40d52df34c3219d286cafbbbb
John Brittain <jsb1982@gmail.com>
2024-09-09 16:16:10 +0100

    Extend Snakemake / Python support window and tests

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,17 @@
-import os
 import nox
 
 
-@nox.session(python="3.11")
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def snakemake7(session):
-    """Test Snakemake 7 with Python 3.11."""
+    """Test Snakemake 7"""
     session.install("snakemake<8", "pulp<2.8", "pytest", ".")
     session.run("pytest")
     session.run("bash", "tests/run_tests.sh", external=True)
 
 
-@nox.session(python="3.12")
+@nox.session(python=["3.12"])
 def snakemake8(session):
-    """Test Snakemake 8 with Python 3.12."""
+    """Test Snakemake 8"""
     session.install("snakemake<9", "pytest", ".")
     session.run("pytest")
     session.run("bash", "tests/run_tests.sh", external=True)
