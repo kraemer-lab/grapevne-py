7daf8768b1cf65670c92d5773109c7b61981b93f
John Brittain <jsb1982@gmail.com>
2024-12-10 17:48:24 +0000

    Port ID renamed to reference to reflect use in Snakefiles

--- grapevne/helpers/helpers.py
+++ grapevne/helpers/helpers.py
@@ -86,15 +86,15 @@ class HelperBase(ABC):
         Port specification:
             [
                 {
-                    id: (required; str),
+                    ref: (required; str),       # Port reference (used in Snakefile)
                     label: (required; str),
                     namespace: (required; str), # Namespace link (incoming)
 
                     # Used for composite modules:
                     mapping: [
                         {                       #   Target module
-                            module: (str),      #     target module name / ID
-                            port: (str),        #     target port id
+                            module: (str),      #     target module name / reference
+                            port: (str),        #     target port reference
 
                         },
                         ...
@@ -115,20 +115,34 @@ class HelperBase(ABC):
             return []
         if isinstance(port, str):
             # Single input port (str)
-            return [{"id": "0", "label": "In", "namespace": port}]
+            return [{"ref": "in", "label": "In", "namespace": port}]
         if isinstance(port, dict):
-            required_keys = ["id", "label", "namespace"]
+            required_keys = ["ref", "label", "namespace"]
             if all(key in port for key in required_keys):
                 # single dict (new format)
                 return [port]
             else:
                 return [
-                    {"id": str(ix), "label": k, "namespace": v}
-                    for ix, (k, v) in enumerate(port.items())
+                    {"ref": k, "label": k, "namespace": v}
+                    for k, v in port.items()
                 ]
         if isinstance(port, list):
             return port
         raise ValueError(f"Unknown port specification: {port}")
+
+    def _get_ports(self, config):
+        """Get ports specification from config"""
+        ports = config.get("ports", None)
+        if ports is None:  # Shorthand / backwards compatibility
+            ports = config.get("input_namespace", None)
+        return self._port_spec(ports)
+
+    def _get_port_namespace(self, ports, port_ref):
+        """Get the namespace for a given port reference"""
+        for port in ports:
+            if port["ref"] == port_ref:
+                return port["namespace"]
+        raise ValueError(f"Port not found: {port_ref}")
 
     # File-type specialisations
 
@@ -152,19 +166,13 @@ class HelperBase(ABC):
             port (str): The name of the input port (optional)
         """
         self._check_config()
-        input_namespace = self.config.get("input_namespace", None)
-        if isinstance(input_namespace, str):
-            if port:
-                raise ValueError("Attempting to index a non-indexable input namespace")
-            indir = self.config["input_namespace"]
-        elif isinstance(input_namespace, dict):
-            indir = self.config["input_namespace"].get(port, None)
-        else:
-            raise ValueError(
-                "Snakemake config error - Input namespace type not recognised"
-            )
-        if not indir:
-            raise ValueError("Attempting to read from a non-configured input port")
+        ports = self._get_ports(self.config)
+        if len(ports) == 0:
+            raise ValueError("No input ports defined in config")
+        if port:  # port ref specified
+            indir = self._get_port_namespace(ports, port)
+        if len(ports) == 1:
+            indir = ports[0]["namespace"]
         if path:
             return f"results/{indir}/{path}"
         else:
