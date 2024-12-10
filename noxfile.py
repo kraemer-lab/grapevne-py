c6b64d3f3d34819fe53c631d31160ef86414dd4d
John Brittain <jsb1982@gmail.com>
2024-12-10 17:19:51 +0000

    Add Python 3.13 to testing framework

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def snakemake7(session):
     """Test Snakemake 7"""
     session.install("snakemake<8", "pulp<2.8", "pytest", ".")
@@ -9,7 +9,7 @@ def snakemake7(session):
     session.run("bash", "tests/run_tests.sh", external=True)
 
 
-@nox.session(python=["3.12"])
+@nox.session(python=["3.12", "3.13"])
 def snakemake8(session):
     """Test Snakemake 8"""
     session.install("snakemake<9", "pytest", ".")
